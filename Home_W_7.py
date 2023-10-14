faa2f49afa91e21e10f601c2134ee99db3933b81
Ivan <ragna.blood82@gmail.com>
2023-10-14 17:52:39 +0300

    Complite

---
+++ Home_W_7.py
@@ -0,0 +1,33 @@
+                                                    # Задание семь
+
+N = int(input("Введите количество элементов списка "))
+
+spisok = list(map(int, input().split()))[:n]
+
+e = set(spisok)
+
+print(len(e))
+
+                                                   # задание второе
+
+a = set(input().split())
+
+b = set(input().split())
+
+print (len(a.intersection(b)))     
+
+                                                  # Третье Задание
+
+a = set()
+
+for i in input().split():
+
+ if i not in a:
+
+  a.add(i)
+
+ print('NO')
+
+else:
+
+ print('YES')                                                  
